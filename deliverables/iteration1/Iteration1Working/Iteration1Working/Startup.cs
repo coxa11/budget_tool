d3b22c359e0e7923ed8e23fbf1aeda189a9d0a32
Rattman <rattmand42@gmail.com>
2016-02-11 13:17:08 -0500

    Added Iteration1Working project

---
+++ deliverables/iteration1/Iteration1Working/Iteration1Working/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Iteration1Working.Startup))]
+namespace Iteration1Working
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
